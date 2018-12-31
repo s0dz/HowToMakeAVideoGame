832f2428c2d4d8c8187ff913ef910fbb0a824728
Garrett McTear <gmctear@jane.com>
2018-12-31 12:51:44 -0700

    Added jump mechanic to PlayerController. Not very smooth.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -16,10 +16,15 @@ namespace Assets.Scripts
         {
             float moveHorizontal = Input.GetAxis("Horizontal");
             float moveVertical = Input.GetAxis("Vertical");
+            bool isJump = Input.GetKeyDown(KeyCode.Space);
 
             Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+            Vector3 jump = new Vector3(0.0f, 10, 0.0f);
 
             rb.AddForce(movement * speed);
+
+            if (isJump)
+                rb.AddForce(jump * speed);
         }
     }
 }
